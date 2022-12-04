e73fedc2cd239cd1e31975e586b514374dae362f
Rehannah <rehannah12345@gmail.com>
2022-12-04 17:04:46 -0400

    load favicon

--- App/views/index.py
+++ App/views/index.py
@@ -3,12 +3,11 @@ from flask_jwt import current_identity
 
 index_views = Blueprint("index_views", __name__, template_folder="../templates")
 
-
 @index_views.route("/", methods=["GET"])
 def index_page():
     return render_template("auth/signup.html", data=None)
 
-# Load Browser Favorite Icon
+# Load Favicon
 @index_views.route('/favicon.ico')
 def favicon():
     return url_for('static', filename='image/favicon.ico')
