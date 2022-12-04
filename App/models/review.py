c31796dd2125771eb7701773ef289486f805201e
Rehannah <rehannah12345@gmail.com>
2022-12-04 17:23:31 -0400

    timestamp format change

--- App/models/review.py
+++ App/models/review.py
@@ -26,7 +26,7 @@ class Review(db.Model):
             "id": self.id,
             "staff_id": self.staff_id,
             "student_id": self.student_id,
-            "timestamp": self.timestamp.strftime("%d/%m/%Y %H:%M")[:-3],
+            "timestamp": self.timestamp.strftime("%d/%m/%Y %H:%M"),
             "sentiment":self.sentiment,
             "text": self.text,
             "num_upvotes": self.get_num_upvotes(),
